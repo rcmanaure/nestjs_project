75670663f82b42a3c40c21ffa06b4b2545eed50a
Ruben Castillo <rcmanaure@gmail.com>
2020-12-15 17:43:04 -0400

    3rd Commit. Added docker-compose file and connected nestjs to a postgres database and made 2 Endpoints to POST and GET data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // instalar para poder usar el process.env, npm i --save @nestjs/config
+    ConfigModule.forRoot({isGlobal: true}),
+    // Para conectar a la base de datos, npm install --save @nestjs/typeorm typeorm pg
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/models/user.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// Modelado de la base de datos
+@Entity()
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name : string;
+}
